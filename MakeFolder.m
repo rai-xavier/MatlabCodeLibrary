80b7d7f15385ef1b7937bd6b3abcd85ef0934a29
rai-xavier <xgutierrez@reality.ai>
2020-05-31 19:22:53 -0400

    extract directory from file paths

--- MakeFolder.m
+++ MakeFolder.m
@@ -1,5 +1,9 @@
 function MakeFolder(folderpath)
+if isempty(folderpath);return;end
+if iscell(folderpath);folderpath=string(folderpath);end
 
+
+[folderpath,thisfn,thisext] = fileparts(folderpath);
 if TerminalMode
     if not(exist(folderpath, 'dir'))
         mkdir(folderpath)
