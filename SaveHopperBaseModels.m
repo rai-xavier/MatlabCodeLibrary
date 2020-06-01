d3d5416003def148dc7dc16747321005f3548017
rai-xavier <xgutierrez@reality.ai>
2020-05-31 22:45:56 -0400

    make basemodels directory

--- SaveHopperBaseModels.m
+++ SaveHopperBaseModels.m
@@ -1,20 +1,33 @@
-function SaveHopperBaseModels(HopperModelPath, varargin)
-[ModelDir,ModelFileName,ext] = fileparts(HopperModelPath);
-if not(isempty(varargin))
-    BaseModelDir = varargin{1};
-else
-    BaseModelDir = fullfile(ModelDir,'BaseModels');
-end
-disp(['Loading ' HopperModelPath])
-H = load(HopperModelPath);
-mat_vars = fields(H);
-idx_h = find(contains(mat_vars,'H'));
-if length(idx_h)>1
-    disp(strcat("Multiple 'H_'-variables found in filepath: ", string(mat_vars(idx_h))));
-    return
-end
-H = H.(mat_vars{idx_h});
+function SaveHopperBaseModels(H, varargin)
 
+if ischar(H)
+    
+    HopperModelPath = H;
+    [ModelDir,ModelFileName,ext] = fileparts(HopperModelPath);
+    if not(isempty(varargin))
+        BaseModelDir = varargin{1};
+    else
+        BaseModelDir = fullfile(ModelDir,'BaseModels');
+    end
+    MakeFolder(BaseModelDir);
+    disp(['Loading ' HopperModelPath])
+    H = load(HopperModelPath);
+    mat_vars = fields(H);
+    idx_h = find(contains(mat_vars,'H'));
+    if length(idx_h)>1
+        syslog(strcat("Multiple 'H_'-variables found in filepath: ", string(mat_vars(idx_h))),'w');
+        return
+    end
+    H = H.(mat_vars{idx_h});
+else
+    if not(isempty(varargin))
+        BaseModelDir = varargin{1};
+        MakeFolder(BaseModelDir);
+    else
+        syslog('Provided Hopper-object without BaseModelDir','w')
+        return
+    end
+end
 % get model names
 ModelTypes = H.HopperModels.Properties.VariableNames;
 for ii=1:length(ModelTypes)
@@ -25,10 +38,10 @@ for ii=1:length(ModelTypes)
         BaseModelName = [ModelFileName '_' ModelTag ext];
         BaseModelPath = fullfile(BaseModelDir, BaseModelName);
         if exist(BaseModelPath)
-            disp(['File already exists (skip) : ' BaseModelPath])
+            syslog(['File already exists (skip) : ' BaseModelPath])
             continue
         end
-        disp(['Saving ' BaseModelPath])
+        syslog(BaseModelPath,'s')
         save(BaseModelPath,'H_BaseModel','-v7.3')
     end
 end
