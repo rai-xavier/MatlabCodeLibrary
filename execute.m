31156df69f06a63d562acab5d6a6be77009de694
rai-xavier <xgutierrez@reality.ai>
2020-06-20 11:28:36 -0400

    script parameters as varargin // process by datatype // parse numerical arrays for eval

--- execute.m
+++ execute.m
@@ -1,6 +1,21 @@
-function execute(script_name)
+function execute(script_name,varargin)
 try
     syslog(strcat("Running  ",script_name))
+    for jj=1:2:length(varargin)
+        define_var_string = strcat(varargin{jj}, " = ");
+        myclass = class(varargin{jj+1});
+        switch myclass
+            case 'double'
+                if length(varargin{jj+1}) ==1
+                    define_var_string = strcat(define_var_string , string(varargin{jj+1}), ";");
+                else
+                    define_var_string = strcat(define_var_string , "[", join(string(1:3)," "),"]", ";");
+                end
+        end
+        
+        syslog(define_var_string);
+        eval(define_var_string)
+    end
     eval(script_name)
 catch ME
     disp(ME)
