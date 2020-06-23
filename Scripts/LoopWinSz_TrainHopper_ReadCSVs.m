214f70b138eb4f0e77d45cba4d76c4b1510f1ce9
rai-xavier <xgutierrez@reality.ai>
2020-06-23 00:49:44 -0400

    apply results helper functions

--- Scripts/LoopWinSz_TrainHopper_ReadCSVs.m
+++ Scripts/LoopWinSz_TrainHopper_ReadCSVs.m
@@ -1,35 +1,39 @@
-addpath('/home/xgutierrez/MatlabCodeLibrary')
+%% Paths
 if TerminalMode
-    projectdir = '/home/xgutierrez/Ripple/';
-    addpath('/home/xgutierrez/HopperUtilities')
+    projectdir = '';
+    
     % Hopper
+    addpath(fullfile(projectdir,'Hopper'))
+    
     % Req'd Vars
     if not(exist('windex'));syslog('Require var windex','x');end
+    
 else
-    clc;clearvars;close('all')
-    projectdir = 'C:\wrk\Ripple\';
-    windex = input('windex = ');
+    clearvars;close('all')
+    projectdir = '';
+    if not(exist('windex')); windex = input('windex = ');    end
 end
-syslog(strcat('windex = ',num2str(windex)))
 
 addpath(projectdir)
+syslog(strcat('windex = ',num2str(windex)))
 
-%% Paths
-projectdir = 'C:\wrk\Ripple\';
 modeldir = fullfile(projectdir,'Results-WinExplore');
 
-csvdir = fullfile(projectdir, '01-Cmpr_Rslts_Test_20200615\Pre-Segmented\');
+csvdir = fullfile(projectdir, '');
 mydir=dir(csvdir);
 
 %% Train Parameters
+
+% subset
 SubsetProportion = 0.3;
 syslog(strcat('SubsetProportion = ',num2str(SubsetProportion)))
 
+% use parallel
 UseParallel = false;
+syslog(strcat('UseParallel = ',num2str(UseParallel)))
 
+% segmentation
 Fs = 20e3;
-Options = struct('samplerate',20e3,'nfilts',26);
-
 winsz_low = 50;
 winsz_hi = 2e3;
 num_win = 6;
@@ -37,10 +41,19 @@ window_sizes = [linspace(winsz_low,winsz_hi,num_win) 2.^(7:10)];
 window_sizes = window_sizes(windex);
 syslog(strcat('window_sizes = ',num2str(window_sizes)))
 
-FeatureSpaces = 'BA1 BA2 BA3 BA4 BA5 BA6 C1 C2 C3 C4 C5 BC1 BC2 BC3 BC4 BC5';
+% feature spaces
+Options = struct('samplerate',Fs,'nfilts',26);
+A = split('A1 A2 A3 A5 A5 A6',' ');
+BA = cellfun(@(x) ['B' x], A,'UniformOutput',false);
+C = split('C1 C2 C3 C4 C5 C6 C7',' ');
+BC = cellfun(@(x) ['B' x], C,'UniformOutput',false);
+E1 = cellfun(@(x) ['E1' x], C,'UniformOutput',false);
+E2 = cellfun(@(x) ['E2' x], C,'UniformOutput',false);
+FeatureSpaces = [A;BA;C;BC]';
 syslog(strcat('FeatureSpaces = ',FeatureSpaces))
 
-ModelTypes = {'STA_LIN_OVO'};
+% model types
+ModelTypes = {'STA_LIN_OVO','STA_LIN_OVA','NST_POLY_OVO'};
 syslog(strcat('ModelTypes = ',join(ModelTypes,', ')))
 
 %% Loop Windows
@@ -51,15 +64,13 @@ for winsz = window_sizes
     
     ModelFileName = fullfile(modeldir,['H_' num2str(winsz) 'win']);
     syslog(strcat('ModelFileName = ',num2str(ModelFileName)))
-
+    
     ClassBalanceFigureFileName = strrep(ModelFileName,'.mat','_ClassBalance.png');
     syslog(strcat('ClassBalanceFigureFileName = ',num2str(ClassBalanceFigureFileName)))
     
-    ResubTableFileName = fullfile(modeldir,'ResultsTables',['Win' num2str(winsz) '_Resub']);
+    ResultsTableFileName = strrep(ModelFileName,'.mat','_ResultsTable.csv');
     syslog(strcat('ResubTableFileName = ',num2str(ResubTableFileName)))
     
-    KFoldTableFileName = fullfile(modeldir,'ResultsTables',['Win' num2str(winsz) '_KFold']);
-    syslog(strcat('KFoldTableFileName = ',num2str(KFoldTableFileName)))
     %% Loop CSVs
     HData = {};
     HLabels = [];
@@ -67,6 +78,7 @@ for winsz = window_sizes
     for ii=1:length(mydir)
         [~,fn,ext]=fileparts(mydir(ii).name);
         if not(contains(ext,'csv'));continue;end
+        
         % read table
         T = readtable(fullfile(csvdir,mydir(ii).name));
         HData{end+1,1} = window_data(T.Volt, winsz,stepsz);
@@ -81,86 +93,88 @@ for winsz = window_sizes
     HLabels = cell2mat(HLabels);
     HGroups = cell2mat(HGroups);
     
-    [HGroups,groupmap] = grp2idx(HGroups);
-    [HLabels,labelmap] = grp2idx(HLabels);
-    
-    syslog(strcat('# Samples = ',num2str(length(HLabels))))
+    [HGroups,GroupMap] = grp2idx(HGroups);
+    [HLabels,ClassMap] = grp2idx(HLabels);
+    syslog(strcat('NumSamples = ',num2str(length(HLabels))))
     
     %% Plot ClassBalance
     
     SetFigureToFullScreen(1);clf
     histogram(HLabels);
     xticks(unique(HLabels))
-    xticklabels(labelmap);
+    xticklabels(ClassMap);
     xtickangle(45)
     ylabel('# Samples')
-    [~,mytitle,~] = fileparts(ClassBalanceFigureFileName);
+    mytitle={};
+    [~,mytitle{1},~] = fileparts(ClassBalanceFigureFileName);
+    mytitle{2} = ['# Total Samples = ' num2str(length(HLabels))];
+    mytitle{3} = [num2str(winsz) 'win'];
     mytitle = strrep(mytitle,'_','-');
-    mytitle = {strrep(mytitle,'_','-')};
-    mytitle{end+1} = ['# Samples = ' num2str(length(HLabels))];
     title(mytitle);
     SetFigureToFullScreen(1);
     saveas(figure(1),ClassBalanceFigureFileName);
     DockFigure(1)
     
     %% Subset Data
- [hdata,edges] = histcounts(HLabels,6);
-    hdatanorm = hdata/max(hdata);
-    HLabelsDscrt = discretize(HLabels,edges);
-    uLabels = unique(HLabelsDscrt);
-    SubsetIdx = {};
-    for jj=1:length(uLabels)
-        idx_class = HLabelsDscrt == uLabels(jj);
-        idx_class = find(idx_class);
-        idx_subset = randperm(length(idx_class),round(SubsetProportion*length(idx_class)));
-        idx_class_subset = idx_class(idx_subset);
-        SubsetIdx{end+1,1} = idx_class_subset;
-    end
-    SubsetIdx = cell2mat(SubsetIdx);
-    HData = cellfun(@(x) x(SubsetIdx,:),HData,'UniformOutput',false);
-    HLabels = HLabels(SubsetIdx);
     
-%     NumOfEachClass = min(hdata);
-%     [HData,~,SubsetIdx] = GET_HOPPER_DATA_SUBSET(HData, HLabelsDscrt, 2);
-%     HLabels = HLabels(SubsetIdx);
+    NumOfEachClass = min(histcounts(HLabels));
+    [HData,HLabels,SubsetIdx] = GET_HOPPER_DATA_SUBSET(HData, HLabels, NumOfEachClass);
+    HGroups = HGroups(SubsetIdx);
     
     ClassBalanceFigureFileName = strrep(ClassBalanceFigureFileName, '.png','_Subset.png');
     SetFigureToFullScreen(2);clf
     histogram(HLabels);
     ylabel('# Samples')
-    [~,mytitle,~] = fileparts(ClassBalanceFigureFileName);
+    mytitle={};
+    [~,mytitle{1},~] = fileparts(ClassBalanceFigureFileName);
+    mytitle{2} = ['# Total Samples = ' num2str(length(HLabels))];
+    mytitle{3} = [num2str(winsz) 'win'];
     mytitle = strrep(mytitle,'_','-');
     title(mytitle);
     SetFigureToFullScreen(2);
     saveas(figure(2),ClassBalanceFigureFileName);
     
-    %% Train Hopper
-    tic
+    %% Initialize Hopper
     H = HopperSVM(...
         'Data', HData, ...
         'MetaData', HLabels, ...
+        'ClassMap', ClassMap,...
         'Groups', HGroups,...
+        'GroupMap',GroupMap,...
         'FeatureNames',FeatureSpaces, ...
         'ModelNames', ModelTypes,...
-        'Options', Options ,...
         'UseParallel', UseParallel);
+    H = H.H_FEATURE_EXTRACTORS();
     
-    % train
+    %% Train - Resub
+    tic
     H = H.H_TRAIN_MODELS(1);
     H = H.H_RESUB();
-    toc
-    syslog(['Total training time = ' num2str(toc/60) ' min'],'t')
-    syslog(['Saving to ' ModelFileName],'s')
-    save(ModelFileName, 'H','labelmap','groupmap')
+    syslog(['Total training time = ' num2str(toc/60) ' min'])
+    syslog(['Saving to ' ModelFileName])
+    save(ModelFileName, 'H')
+    ResultsTable = getClassifyResultsTable(H.HopperModels);
+    writetable(ResultsTable,ResultsTableFileName)
     
-    % kfold
+    %% Train - KFold
     rng(0);
-    tic; H = H.H_KFOLD([], 10, [], true); 
-    syslog(['Total kfold time = ' num2str(toc/60) ' min'],'t')
-    syslog(['Saving to ' ModelFileName],'s')
-    save(ModelFileName, 'H','labelmap','groupmap')
-    
-    %% Store results
+    tic
+    H = H.H_KFOLD([], 10, [], true); toc
+    syslog(['Total kfold time = ' num2str(toc/60) ' min'])
+    syslog(['Saving to ' ModelFileName])
+    save(ModelFileName, 'H')
+    ResultsTable = getClassifyResultsTable(H.HopperModels);
+    writetable(ResultsTable,ResultsTableFileName)
+    for ii=1:lenght(ModelTypes)
+        for jj=1:length(FeatureSpaces)
+            ConfusionMat = getHopperFeatConfMat(H.HopperModels,ModelTypes{ii},FeatureSpaces{jj},'kfold');
+            saveConfMatFig(ConfusionMat, ModelFilePath, labelmap, FeatureSpaces{jj}, 'kfold');
+            [Recall,Precision,F1] = getMarginals(ConfusionMat);
+        end
+    end
     
     %% end loop windows
 end
+
+%% Emd of script
+syslog('Script complete.','x')
