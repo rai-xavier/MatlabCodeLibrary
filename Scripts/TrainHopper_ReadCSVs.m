f12d9ab557e382a050e4a4a28a6486bc0c1d89f8
rai-xavier <xgutierrez@reality.ai>
2020-06-23 00:31:56 -0400

    Implement results helper funcns // define feature space groups

--- Scripts/TrainHopper_ReadCSVs.m
+++ Scripts/TrainHopper_ReadCSVs.m
@@ -1,41 +1,48 @@
 
 %% Paths
-projectdir = 'C:\wrk\Ripple\';
-
-csvdir = fullfile(projectdir, '01-Cmpr_Rslts_Test_20200615\Pre-Segmented\');
-mydir=dir(csvdir);
+projectdir = '';
+csvdir = fullfile(projectdir, '');
 
 modeldir = fullfile(projectdir,'Results');
 ModelFileName = fullfile(modeldir,'');
 ClassBalanceFigureFileName = strrep(ModelFileName,'.mat','_ClassBalance.png');
+ResubTableFileName = strrep(ModelFileName,'.mat','_ResultsTable.csv');
 
 %% Train Parameters
 Fs = 20e3;
-winsz = ;
+winsz = 0.5*Fs;
 stepsz = 0.5*winsz;
-FeatureSpaces = 'A1 A2 A3 A4 A5 A6 BA1 BA2 BA3 BA4 BA5 BA6 C1 C2 C3 C4 C5 BC1 BC2 BC3 BC4 BC5';
-ModelTypes = {'STA_LIN_OVO'};
+A = split('A1 A2 A3 A5 A5 A6',' ');
+BA = cellfun(@(x) ['B' x], A,'UniformOutput',false);
+C = split('C1 C2 C3 C4 C5 C6 C7',' ');
+BC = cellfun(@(x) ['B' x], C,'UniformOutput',false);
+E1 = cellfun(@(x) ['E1' x], C,'UniformOutput',false);
+E2 = cellfun(@(x) ['E2' x], C,'UniformOutput',false);
+FeatureSpaces = [A;BA;C;BC]';
+ModelTypes = {'STA_LIN_OVO','STA_LIN_OVA','NST_POLY_OVO'};
 
 %% Loop CSVs
 HData = {};
 HLabels = [];
-
+mydir=dir(csvdir);
 for ii=1:length(mydir)
-  [~,fn,ext]=fileparts(mydir(ii).name);
+    [~,fn,ext]=fileparts(mydir(ii).name);
     if not(contains(ext,'csv'));continue;end
+    
     % read table
-   T = readtable(fullfile(csvdir,mydir(ii).name));
-   HData{end+1,1} = window_data(T.,winsz,stepsz);
-   HLabels{end+1,1} = window_data(T.,winsz,stepsz);
-   HLabels{end} = mean(HLabels{end},2);
-   
+    T = readtable(fullfile(csvdir,mydir(ii).name));
+    HData{end+1,1} = window_data(T.Data,winsz,stepsz);
+    HLabels{end+1,1} = window_data(T.Labels,winsz,stepsz);
+    HLabels{end} = mean(HLabels{end},2);
+    
 end
+
 HData = {cell2mat(HData)};
 HLabels = cell2mat(HLabels);
 HGroups = cell2mat(HGroups);
 
-[HGroups,groupmap] = grp2idx(HGroups);
-[HLabels,labelmap] = grp2idx(HLabels);
+[HGroups,GroupMap] = grp2idx(HGroups);
+[HLabels,ClassMap] = grp2idx(HLabels);
 
 %% ClassBalance
 
@@ -57,27 +64,41 @@ hdata = histcounts(HLabels);
 numOfEachClass = min(hdata);
 [HData,HLabels,SubsetIdx] = GET_HOPPER_DATA_SUBSET(HData, HLabels, NumOfEachClass);
 
-%% Train Hopper
-tic
+%% Initialize Hopper
 H = HopperSVM(...
     'Data', HData, ...
     'MetaData', HLabels, ...
+    'ClassMap', ClassMap,...
     'Groups', HGroups,...
+    'GroupMap',GroupMap,...
     'FeatureNames',FeatureSpaces, ...
     'ModelNames', ModelTypes,...
     'UseParallel', false);
+H = H.H_FEATURE_EXTRACTORS();
 
-% train
+%% Train - Resub
+tic
 H = H.H_TRAIN_MODELS(1);
 H = H.H_RESUB();
-toc
 disp(['Total training time = ' num2str(toc/60) ' min'])
 disp(['Saving to ' ModelFileName])
-save(ModelFileName, 'H','labelmap','groupmap')
-rng(0);
+save(ModelFileName, 'H')
+ResultsTable = getClassifyResultsTable(H.HopperModels);
+writetable(ResultsTable,ResubTableFileName)
 
-% kfold
-tic; H = H.H_KFOLD([], 10, [], true); toc
+%% Train - KFold
+rng(0);
+tic
+H = H.H_KFOLD([], 10, [], true); toc
 disp(['Total kfold time = ' num2str(toc/60) ' min'])
 disp(['Saving to ' ModelFileName])
-save(ModelFileName, 'H','labelmap','groupmap')
+save(ModelFileName, 'H')
+ResultsTable = getClassifyResultsTable(H.HopperModels);
+writetable(ResultsTable,ResubTableFileName)
+for ii=1:lenght(ModelTypes)
+    for jj=1:length(FeatureSpaces)
+        ConfusionMat = getHopperFeatConfMat(H.HopperModels,ModelTypes{ii},FeatureSpaces{jj},'kfold');
+        saveConfMatFig(ConfusionMat, ModelFilePath, labelmap, FeatureSpaces{jj}, 'kfold');
+        [Recall,Precision,F1] = getMarginals(ConfusionMat);
+    end
+end
