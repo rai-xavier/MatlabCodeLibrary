8eb9d8e0c4353d2628f99455a031330347da1253
rai-xavier <xgutierrez@reality.ai>
2020-06-26 12:19:30 -0400

    change func name // pass fig obj title as arg

---
+++ dock.m
@@ -0,0 +1,37 @@
+function dock(varargin)
+if TerminalMode;    return;     end
+
+try
+    if nargin>=1
+        switch length(varargin)
+            case 1
+                if isnumeric(varargin{1})
+                    fignum = varargin{1};
+                    f = figure(fignum);
+                elseif ischar(varargin{1})
+                    figname = varargin{1};
+                    f = figure();
+                    set(f,'Name',figname,'NumberTitle','off');
+                elseif isa(varargin{1},'matlab.ui.Figure')
+                    f = varargin{1};
+                end
+                
+            case 2
+                fignum = varargin{1};
+                figname = varargin{2};
+                f = figure(fignum);
+                set(f,'Name',figname,'NumberTitle','off');
+        end
+    else
+       f = figure();        
+    end
+        
+    
+    set(f,'WindowStyle','docked');
+    drawnow
+
+catch ME
+    
+end
+return
+end
