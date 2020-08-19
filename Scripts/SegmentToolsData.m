6ae1f23fdf3d19d293beafeca014bfe3c4ea93a3
rai-xavier <xgutierrez@reality.ai>
2020-08-19 00:34:04 -0400

    segment hd5 using indexcsv and write to csv

--- Scripts/SegmentToolsData.m
+++
@@ -1,4 +0,0 @@
-jsondir = '';
-CSVData = readtable(fullfile(jsondir,'.csv'));
-RawData = h5read(fullfile(jsondir,'.hd5'), '/dataset0')';
-[HData, HLabels, HGroups, HIdx] =  ConvertDataFromHD5andCSVToHopperFormat(RawData, CSVData, [], 256, 'BroseRadar');
