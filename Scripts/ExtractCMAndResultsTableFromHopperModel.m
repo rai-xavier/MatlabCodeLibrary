7e6574577a99dce2ca30bab07a61c4b8f89eefcd
rai-xavier <xgutierrez@reality.ai>
2020-08-28 14:30:50 -0400

    flags to control extract

--- Scripts/ExtractCMAndResultsTableFromHopperModel.m
+++ Scripts/ExtractCMAndResultsTableFromHopperModel.m
@@ -1,8 +1,24 @@
-srcdir = 'C:\wrk\Ripple\Deliverables\SourceFiles\';
-modeldir = fullfile(srcdir,'Models'); 
-basemodeldir = fullfile(srcdir,'BaseModels');
-confmatdir = fullfile(srcdir,'ConfMats');
-resultstabledir =  fullfile(srcdir,'ResultTables');
+% srcdir = 'C:\wrk\Ripple\Deliverables\SourceFiles\';
+srcdir = 'C:\wrk\Ripple\Deliverables\HallCounter';
+modeldir = fullfile(srcdir,'Models');
+
+%% base model
+basemodelflag = true;
+if basemodelflag
+    basemodeldir = fullfile(srcdir,'BaseModels');
+end
+
+%% confmat
+confmatflag = false;
+if confmatflag
+    confmatdir = fullfile(srcdir,'ConfMats');
+end
+
+%% resultstable
+resultstableflag = false;
+if resultstableflag
+    resultstabledir =  fullfile(srcdir,'ResultTables');
+end
 
 %% loop through files
 mydir=dir(modeldir);
@@ -10,15 +26,15 @@ for i = 1:length(mydir)
     [~,fn,ext]=fileparts(mydir(i).name);
     if not(contains(ext,'mat'));continue;end
     
+    
+    %% load model
     ModelName = [fn ext];
     disp(ModelName)
     ModelFilePath = fullfile(modeldir,ModelName);
-    ResultsTableFilePath = fullfile(resultstabledir,strrep(ModelName,'.mat','-ResultTable.csv'));
-
     load(ModelFilePath,'H')
-
     
-    %% Save ResubCM
+    
+    %% CM/BaseModel
     Models = H.HopperModels.Properties.VariableNames;
     Models = reshape(Models,1,length(Models));
     Feats = H.HopperModels.Properties.RowNames;
@@ -30,35 +46,43 @@ for i = 1:length(mydir)
             
             BaseModelName = strrep(ModelName,'.mat',['-' M '-' F '.mat']);
             
-            BaseModelPath = fullfile(basemodeldir,BaseModelName);
-            HBaseModel = H.HopperModels.(M).BaseModel;
-            save(BaseModelPath, 'HBaseModel','-v7.3')
+            if basemodelflag
+                BaseModelPath = fullfile(basemodeldir,BaseModelName)
+                HBaseModel = H.HopperModels.(M)(f).BaseModel;
+                save(BaseModelPath, 'HBaseModel','-v7.3')
+            end
             
-            ConfMatPath = fullfile(confmatdir,strrep(BaseModelName,'.mat','-ResubCM.png'));
-            dock(strrep(BaseModelName,'.mat',''))
-            plotConfMat(H.HopperModels.(M)(f).ResubConfusionMat,unique(H.MetaData))
-            title(strrep(BaseModelName,'_','-'))
-            saveas(gcf,ConfMatPath);
+            if confmatflag
+                ConfMatPath = fullfile(confmatdir,strrep(BaseModelName,'.mat','-ResubCM.png'));
+                dock(strrep(BaseModelName,'.mat',''))
+                plotConfMat(H.HopperModels.(M)(f).ResubConfusionMat,unique(H.MetaData))
+                title(strrep(BaseModelName,'_','-'))
+                saveas(gcf,ConfMatPath);
+            end
         end
     end
-      
-    %% Add MetaData & Save ResultsTable
-    ResultsTable = getClassifyResultsTable(H.HopperModels);
-    writetable(ResultsTable,ResultsTableFilePath)
     
+    %% Add MetaData & Save ResultsTable
+    if resultstableflag
+        ResultsTableFilePath = fullfile(resultstabledir,strrep(ModelName,'.mat','-ResultTable.csv'));
+        ResultsTable = getClassifyResultsTable(H.HopperModels);
+        writetable(ResultsTable,ResultsTableFilePath)
+    end
     %% done
     disp('')
     close all
 end
 
 %%
-MasterResults = table;
-mydir = dir(resultstabledir);
-for i = 1:length(mydir)
-    [~,fn,ext]=fileparts(mydir(i).name);
-    if not(contains(ext,'csv'));continue;end
-    T = readtable(fullfile(resultstabledir,mydir(i).name));
-    MasterResults=[MasterResults;T];
+if resultstableflag
+    MasterResults = table;
+    mydir = dir(resultstabledir);
+    for i = 1:length(mydir)
+        [~,fn,ext]=fileparts(mydir(i).name);
+        if not(contains(ext,'csv'));continue;end
+        T = readtable(fullfile(resultstabledir,mydir(i).name));
+        MasterResults=[MasterResults;T];
+    end
+    MasterResults = sortrows(MasterResults,'Accuracy','descend');
+    % writetable(MasterResults,fullfile(resultstabledir,'MasterResults.csv'))
 end
-MasterResults = sortrows(MasterResults,'Accuracy','descend');
-writetable(MasterResults,fullfile(resultstabledir,'MasterResults.csv'))
