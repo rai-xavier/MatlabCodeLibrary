740acbb98d3c2a4da2ca76f5e12ebf7d5138c343
rai-xavier <xgutierrez@reality.ai>
2020-09-22 13:24:04 -0400

    rename FrameRate to FPS

--- plot-figs/SaveFrameToGIF.m
+++ plot-figs/SaveFrameToGIF.m
@@ -1,4 +1,4 @@
-function SaveFrameToGIF(fignum,framenum, fn_gif, FrameRate)
+function SaveFrameToGIF(fignum,framenum, fn_gif, FPS)
 
 frame = getframe(fignum);
 
@@ -6,9 +6,9 @@ im = frame2im(frame);
 [imind,cm] = rgb2ind(im,256);
 
 if framenum == 1
-    imwrite(imind,cm,fn_gif,'gif', 'Loopcount',inf,'DelayTime',1/FrameRate);
+    imwrite(imind,cm,fn_gif,'gif', 'Loopcount',inf,'DelayTime',1/FPS);
 else
-    imwrite(imind,cm,fn_gif,'gif','WriteMode','append','DelayTime',1/FrameRate);
+    imwrite(imind,cm,fn_gif,'gif','WriteMode','append','DelayTime',1/FPS);
 end
 
 return
