56f97e4be1fa4fd7d8c23da17a3ff61db564843c
rai-xavier <xgutierrez@reality.ai>
2020-09-22 13:21:57 -0400

    optionally run hopper-log-plots

--- hopper/scripts/RunHopperController.m
+++ hopper/scripts/RunHopperController.m
@@ -1,10 +1,10 @@
-ProcessIdentifier = 'DEBUG-h2go';
+ProcessIdentifier = 'DEBUG';
 pathToPoolProfile = '\\abyss1\active_projects\Hopper_Dev\dev-xavier\Hopper\Cloud_Cluster_Profile.settings';
 clc
 %% pathToJsonInit
-baseToolPath = 'Z:\Rheem\IndoorPrognostics\Phase 1 Analysis\Sensor-Sensitivity\A1-2500';
-pathToJsonInit = fullfile(baseToolPath, 'jsonString.json');
-%% format jsonCommand 
+baseToolPath = 'Z:\Hopper_Dev\dev-xavier\ToolsDebug\9-22 NoHData';
+pathToJsonInit = fullfile(baseToolPath, 'jsonString_local.json');
+%% format jsonCommand
 jsonCommand = struct;
 jsonCommand.MethodToUse = 'JSONINIT';
 jsonCommand.MethodParameters = pathToJsonInit;
@@ -21,24 +21,26 @@ diary on
 HopperController(ProcessIdentifier, pathToPoolProfile, jsonCommand)
 diary off
 %% Inspect log/diary
-hopperLogArray = ReadHopperLog(diaryfile);
-openvar('hopperLogArray')
-
-start_task =  hopperLogArray.Message( contains( hopperLogArray.Message, 'Executing JSON task') );
-% NumTasks = sum(start_task);
-
-NoiseValues = hopperLogArray.Value( contains( hopperLogArray.Message, 'Noise') );
-NoiseValues = cell2mat(NoiseValues);
-[NoiseValues,sortidx] = sort(NoiseValues);
-
-RESULT_TABLE =  hopperLogArray.Value( contains( hopperLogArray.MessageType, 'RESULT_TABLE') );
-
-Resub_Acc = cellfun(@(result_table) result_table.Resub_Acc, RESULT_TABLE);
-Resub_Acc = Resub_Acc(sortidx);
-
-
-dock
-plot(NoiseValues, Resub_Acc, '-*'); xticks( NoiseValues ); ylim([ 0 1]); ylabel('Resub Acc'); xlabel('NoiseValues')
-mytitle = split(baseToolPath,'\');
-mytitle = mytitle{end};
-title(mytitle)
+if false
+    hopperLogArray = ReadHopperLog(diaryfile);
+    openvar('hopperLogArray')
+    
+    start_task =  hopperLogArray.Message( contains( hopperLogArray.Message, 'Executing JSON task') );
+    % NumTasks = sum(start_task);
+    
+    NoiseValues = hopperLogArray.Value( contains( hopperLogArray.Message, 'Noise') );
+    NoiseValues = cell2mat(NoiseValues);
+    [NoiseValues,sortidx] = sort(NoiseValues);
+    
+    RESULT_TABLE =  hopperLogArray.Value( contains( hopperLogArray.MessageType, 'RESULT_TABLE') );
+    
+    Resub_Acc = cellfun(@(result_table) result_table.Resub_Acc, RESULT_TABLE);
+    Resub_Acc = Resub_Acc(sortidx);
+    
+    
+    dock
+    plot(NoiseValues, Resub_Acc, '-*'); xticks( NoiseValues ); ylim([ 0 1]); ylabel('Resub Acc'); xlabel('NoiseValues')
+    mytitle = split(baseToolPath,'\');
+    mytitle = mytitle{end};
+    title(mytitle)
+end
