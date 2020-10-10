d4b8c56a68c00a4318221ca69b0a5e37f5035bb4
rai-xavier <xgutierrez@reality.ai>
2020-10-10 16:03:48 -0400

    name figure option

--- plot-figs/fullscreen.m
+++ plot-figs/fullscreen.m
@@ -1,7 +1,32 @@
-function fullscreen(varargin)
-if not(isempty(varargin))
-    fignum = varargin{1};
-    f=figure(fignum);
+function varargout = fullscreen(varargin)
+varargout={};
+
+% if not(isempty(varargin))
+%     fignum = varargin{1};
+%     f=figure(fignum);
+% else
+%     f = figure();
+% end
+if nargin>=1
+    switch length(varargin)
+        case 1
+            if isnumeric(varargin{1})
+                fignum = varargin{1};
+                f = figure(fignum);
+            elseif ischar(varargin{1})
+                figname = varargin{1};
+                f = figure();
+                set(f,'Name',figname,'NumberTitle','off');
+            elseif isa(varargin{1},'matlab.ui.Figure')
+                f = varargin{1};
+            end
+            
+        case 2
+            fignum = varargin{1};
+            figname = varargin{2};
+            f = figure(fignum);
+            set(f,'Name',figname,'NumberTitle','off');
+    end
 else
     f = figure();
 end
@@ -9,5 +34,7 @@ set(f,'WindowStyle','normal')
 set(f, 'units','normalized')
 set(f,'outerposition',[0 0 1 1])
 drawnow
+varargout{1} = f;
+
 return
 end
