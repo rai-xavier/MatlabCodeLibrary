768221eca44d64250dcc1c235b2c44e1f3e07e70
rai-xavier <xgutierrez@reality.ai>
2021-02-09 09:29:14 -0500

    update/improve

--- hopper/scripts/RunHopperFullExplore.m
+++ hopper/scripts/RunHopperFullExplore.m
@@ -31,14 +31,18 @@ mydir = dir(datadir);
 %% Dst Directories
 resultsdir = fullfile(projectdir,'Results');
 modeldir = fullfile(resultsdir,'Models');
-basemodeldir = fullfile(resultsdir, 'BaseModels');
+hsvmdir = fullfile(resultsdir, 'HSVM');
 classbalancedir = fullfile(resultsdir, 'ClassBalance');
+rmsdir = fullfile(resultsdir,'RMS');
 resultstabledir = fullfile(resultsdir, 'ResultsTable' );
 confmatdir = fullfile(resultsdir, 'ConfMats');
-MakeFolder(resultsdir,modeldir,basemodeldir,classbalancedir,resultstabledir,confmatdir)
+MakeFolder(resultsdir,modeldir,hsvmdir,classbalancedir,rmsdir,resultstabledir,confmatdir)
 %% Hopper Params
+ModelType = 'STA_LIN_OVO';
+% kfold
 NumGroups = 10;
 UseGroups = true;
+% feature-space
 % A = split('A1 A2 A3 A4 A5 A6',' ');
 % BA = cellfun(@(x) ['B' x], A,'UniformOutput',false);
 C = split('C1 C2 C3 C4 C5 C6 C7',' ');
@@ -46,172 +50,249 @@ BC = cellfun(@(x) ['B' x], C,'UniformOutput',false);
 E1 = cellfun(@(x) ['E1' x], C,'UniformOutput',false);
 E2 = cellfun(@(x) ['E2' x], C,'UniformOutput',false);
 FeatSpaces = [C;BC;E1;E2]';
-if exist('FIndex','var'); FeatSpaces = FeatSpaces(FIndex); end
-ModelType = 'STA_LIN_OVO';
+if exist('FIndexes','var'); FeatSpaces = FeatSpaces(FIndexes); end
+
 %% Data Params
-WindowSizes = 2.^(11:14);
-if exist('WIndex','var'); WindowSizes = WindowSizes(WIndex); end
+% WindowSizes = 2.^(11:14);
+WinSzSeconds = [0.25 0.5 1 2];
+WinSzs = 2.^nextpow2(WinSzSeconds*SampleRate);
+if exist('WIndexes','var'); WinSzs = WinSzs(WIndexes); end
 SubsetProportion = 0.3;
-%% Loop Windows
-for WinSz = WindowSizes
+%% Loop Windows + Feats
+syslog(strcat("WinSzSeconds = ", char(join(string(WinSzSeconds),', '))))
+syslog(strcat("FeatSpaces = ", char(join(FeatSpaces,', '))))
+for WinSz = WinSzs
     StepSz = round( 0.5 * WinSz );
+    Options = struct('SampleRate', SampleRate, 'NumFilts', 0.5);
+    for FeatSpace = FeatSpaces
+        FeatSpace = char(FeatSpace);
+        %% FilePaths + figs
+        DataName = ['D-W' num2str(WinSz)];
+        HSVMName = ['H-W' num2str(WinSz) '-' FeatSpace];
+        ModelFilePath = fullfile(modeldir,HSVMName);
+        TrainResultsTableFilePath = fullfile(resultstabledir, [HSVMName '_TrainResultsTable.csv'] );
+        if and( exist(ModelFilePath,'file'), exist(TrainResultsTableFilePath,'file') );continue; end
+        syslog(strcat("ModelName = ",HSVMName))
+        % continue
+        SegmentDataPath = fullfile(resultsdir,DataName);
+        ClassBalanceFigureFilePath = fullfile(classbalancedir, [ HSVMName '-ClassBalance.png'] );
+        fig_classbalance = dock(1,'ClassBalance');clf
+        RMSFigFilePath = fullfile(rmsdir, [DataName '-RMS.png']);
+        fig_rms = dock(3,'RMS');clf
+        fig_confmat = dock(2,'ConfMat');clf
+        %% Loop DataDir
+        HData = {};
+        HLabels = {};
+        HGroups = {};
+        HGroupsExt = {};
+        for ii=1:length(mydir)
+            [~,fn,ext]=fileparts(mydir(ii).name);
+            if not(contains(ext,'wav'));continue;end
+            % current label
+            ispresencedetection = or( contains(fn, 'Absence'), contains(fn,'Presence') );
+            if ispresencedetection
+                label = 'NotBabyCry';
+            else
+                label = 'BabyCry';
+            end
+            %         if and( strcmp(label,'NotBabyCry'), any(strcmp(vertcat(HLabels{:}),'NotBabyCry')) ); continue; end
+            %         if and( strcmp(label,'BabyCry'), any(strcmp(vertcat(HLabels{:}),'BabyCry')) ); continue; end
+            % read file
+            [y,fs] = audioread(fullfile(datadir,mydir(ii).name));
+            % segment data
+            HData{end+1,1} = window_data(y, WinSz, StepSz);
+            % segment labels
+            HLabels{end+1,1} = repmat(string(label), size(HData{end},1),1);
+            % segment groups
+            HGroups{end+1,1} = repmat(string(fn),size(HData{end},1),1);
+            % segment super-groups
+            HGroupsExt{end+1,1} = join([HLabels{end} HGroups{end}],'-');
+        end
         
-    %% FilePaths
-    ModelName = ['H-W' num2str(WinSz) '.mat'];
-    ModelFilePath = fullfile(modeldir,ModelName);
-    TrainResultsTableFilePath = fullfile(resultstabledir, strrep(ModelName,'.mat','_TrainResultsTable.csv') );
-    if and( exist(ModelFilePath,'file'), exist(TrainResultsTableFilePath,'file') );continue; end
-    syslog(strcat("ModelName = ",ModelName))
-
-    ClassBalanceFigureFilePath = fullfile(classbalancedir, strrep(ModelName,'.mat','_ClassBalance.png') );
-    fig_classbalance = dock(1,'ClassBalance');clf
-    fig_confmat = dock(2,'ConfMat');clf
-    %% Loop DataDir
-    HData = {};
-    HLabels = {};
-    HGroups = {};
-    HGroupsExt = {};
-    for ii=1:length(mydir)
-        [~,fn,ext]=fileparts(mydir(ii).name);
-        if not(contains(ext,'wav'));continue;end
-        % current label
-        ispresencedetection = or( contains(fn, 'Absence'), contains(fn,'Presence') );
-        if ispresencedetection
-            label = 'NotBabyCry';
-        else
-            label = 'BabyCry';
+        %% Format Data
+        
+        % stack
+        for jj=1:size(HData,2)
+            HData(1,jj) = {vertcat(HData{:,jj})};
         end
-%         if and( strcmp(label,'NotBabyCry'), any(strcmp(vertcat(HLabels{:}),'NotBabyCry')) ); continue; end
-%         if and( strcmp(label,'BabyCry'), any(strcmp(vertcat(HLabels{:}),'BabyCry')) ); continue; end
-        % read file
-        [y,fs] = audioread(fullfile(datadir,mydir(ii).name));
-        % segment data
-        HData{end+1,1} = window_data(y, WinSz, StepSz);
-        % segment labels
-        HLabels{end+1,1} = repmat(string(label), size(HData{end},1),1);
-        % segment groups
-        HGroups{end+1,1} = repmat(string(fn),size(HData{end},1),1);
-        % segment super-groups
-        HGroupsExt{end+1,1} = join([HLabels{end} HGroups{end}],'-');
-    end
-    
-    %% Format Data
-    
-    % stack
-    for jj=1:size(HData,2)
-        HData(1,jj) = {vertcat(HData{:,jj})};
-    end
-    HData(2:end,:) = [];
-    HLabels = vertcat(HLabels{:});
-    HGroups = vertcat(HGroups{:});
-    syslog(strcat('NumSamples = ',num2str(length(HLabels))))
-    % map
-    [HGroups,GroupMap] = grp2idx(HGroups);
-    [HLabels,ClassMap] = grp2idx(HLabels);
-    UseParallel = length(ClassMap) >= 3;
-    
-    %% Plot Full ClassBalance
-    figure(fig_classbalance);
-    subplot(211);
-    histogram(HLabels);
-    xticks(unique(HLabels))
-    xticklabels(ClassMap);
-    xtickangle(45)
-    ylabel('# Samples')
-    mytitle={};
-    mytitle{end+1} = ['# Total Samples = ' num2str(length(HLabels))];
-    title(mytitle);
-    titleabove(ModelName);
-    saveas(gcf,ClassBalanceFigureFilePath);
-    
-    %% Subset Data (HLabels vs HGroupsExt)
-    NumOfEachClass = min(histcounts(HLabels));
-    NumOfEachClass = round( SubsetProportion * NumOfEachClass );
-    [HData,HLabels,SubsetIdx] = GET_HOPPER_DATA_SUBSET(HData, HLabels, NumOfEachClass);
-    HGroups = HGroups(SubsetIdx);
-    
-    %% Plot Subset ClassBalance
-    figure(fig_classbalance);
-    subplot(212);
-    histogram(HLabels);
-    ylabel('# Samples')
-    mytitle={};
-    mytitle{2} = ['# Total Samples = ' num2str(length(HLabels))];
-    title(mytitle);
-    titleabove(ModelName);
-    saveas(gcf,ClassBalanceFigureFilePath);
-    
-    %% Run Hopper
-    H = HopperSVM(...
-        'Data', HData, ...
-        'MetaData', HLabels, ...
-        'ClassMap', ClassMap,...
-        'Groups', HGroups,...
-        'GroupMap',GroupMap,...
-        'FeatureNames',FeatSpaces, ...
-        'ModelNames', ModelType,...
-        'UseParallel', UseParallel);
-    H = H.H_FEATURE_EXTRACTORS();
-    clear HData HLabels ClassMap HGroups GroupMap
-    
-    % Train - Resub
-    tic
-    H = H.H_TRAIN_MODELS(1);
-    H = H.H_RESUB();
-    syslog(['Total training time = ' num2str(toc/60) ' min'])
-    
-    % Train - KFold
-    rng(0);
-    tic
-    H = H.H_KFOLD([], NumGroups, [], UseGroups);
-    syslog(['Total kfold time = ' num2str(toc/60) ' min'])   
-    
-    % save Model 
-    syslog(['Saving to ' ModelFilePath])
-    save(ModelFilePath, 'H','-v7.3')
-    
-    % Save BaseModel + CM
-    Models = H.HopperModels.Properties.VariableNames;
-    Models = reshape(Models,1,length(Models));
-    Feats = H.HopperModels.Properties.RowNames;
-    Feats = reshape(Feats,1,length(Feats));
-    for M = Models
-        M = char(M);
-        for f = 1:length(Feats)
-            F = Feats{f};
-            % base model
-            HBaseModel = H.HopperModels.(M).BaseModel;
-            BaseModelName = strrep(ModelName,'.mat',['-' strrep(M,'_','-') '-' F '.mat']);
-            BaseModelPath = fullfile(basemodeldir,BaseModelName);
-            save(BaseModelPath, 'HBaseModel','-v7.3')
-            % resub conf mat
-            ResubConfMatPath = fullfile(confmatdir,strrep(BaseModelName,'.mat','-ResubCM.png'));
-            figure(fig_confmat);clf
-            ResubConfMat = H.HopperModels.(M)(f).ResubConfusionMat;
-            plotConfMat(ResubConfMat,unique(H.MetaData))
-            titleabove({BaseModelName,'Resub'})
-            saveas(gcf,ResubConfMatPath);
-            % kfold conf mat
-            KFoldConfMatPath = fullfile(confmatdir,strrep(BaseModelName,'.mat','-KFoldCM.png'));
-            figure(fig_confmat);clf
-            KFoldConfMat = H.HopperModels.(M)(f).KFoldConfusionMat;
-            plotConfMat(KFoldConfMat,unique(H.MetaData))
-            titleabove({BaseModelName,'KFold'})
-            saveas(gcf,KFoldConfMatPath);
-
+        HData(2:end,:) = [];
+        HLabels = vertcat(HLabels{:});
+        HGroups = vertcat(HGroups{:});
+        syslog(strcat('NumSamples = ',num2str(length(HLabels))))
+        % map
+        [HGroups,GroupMap] = grp2idx(HGroups);
+        [HLabels,ClassMap] = grp2idx(HLabels);
+        UseParallel = length(ClassMap) >= 3;
+        
+        %% Save Full Data
+        save(SegmentDataPath,'HData','HLabels','HGroups','HGroupsExt','ClassMap','GroupMap','GroupExtMap','-v7.3')
+        
+        %% Plot Full ClassBalance
+        figure(fig_classbalance);
+        subplot(211);
+        histogram(HLabels);
+        xticks(unique(HLabels))
+        xticklabels(ClassMap);
+        xtickangle(45)
+        ylabel('# Samples')
+        mytitle={};
+        mytitle{end+1} = ['# Total Samples = ' num2str(length(HLabels))];
+        title(mytitle);
+        titleabove(HSVMName);
+        saveas(gcf,ClassBalanceFigureFilePath);
+        
+        %% RMS By Class
+        figure(fig_rms); clf
+        [nrows,ncols] = sgdims(length(ClassMap));
+        for n = 1 : length(ClassMap)
+            % subset class
+            classidx = find(HLabels == n);
+            % calculate rms
+            ClassRMS = cellfun(@(x) rms(x(classidx,:),2), HData, 'UniformOutput',false);
+            % plot histogram
+            subplot(nrows,ncols,n)
+            h = histogram(ClassRMS{1},NumBins);
+            if not(TerminalMode); xline(mean(ClassRMS{1}),'k'); end
+            title(ClassMap{n})
+            xlabel('RMS')
+            ylabel('# windows')
         end
+        titleabove( { ['win = ' num2str(WinSz)], ['fs = ' num2str(SampleRate)]})
+        saveas(fig_rms,RMSFigFilePath);
+        
+        %% Subset Data (HLabels vs HGroupsExt)
+        NumOfEachClass = min(histcounts(HLabels));
+        NumOfEachClass = round( SubsetProportion * NumOfEachClass );
+        [HData,HLabels,SubsetIdx] = GET_HOPPER_DATA_SUBSET(HData, HLabels, NumOfEachClass);
+        HGroups = HGroups(SubsetIdx);
+        
+        %% Plot Subset ClassBalance
+        figure(fig_classbalance);
+        subplot(212);
+        histogram(HLabels);
+        xticks(unique(HLabels))
+        xticklabels(ClassMap);
+        xtickangle(45)
+        ylabel('# Samples')
+        mytitle={};
+        mytitle{2} = ['# Total Samples = ' num2str(length(HLabels))];
+        title(mytitle);
+        titleabove(HSVMName);
+        saveas(gcf,ClassBalanceFigureFilePath);
+        
+        %% RMS By Class (Submap)
+        figure(fig_rms); clf
+        [nrows,ncols] = sgdims(length(ClassMap));
+        for n = 1 : length(ClassMap)
+            classidx = HLabels == n;
+            ClassRMS = cellfun(@(x) rms(x(classidx,:),2), HData, 'UniformOutput',false);
+            subplot(nrows,ncols,n)
+            histogram(ClassRMS{1},20)
+            if not(TerminalMode); xline(mean(ClassRMS{1})); end
+            title(ClassMap{n})
+            xlabel('RMS')
+            ylabel('# windows')
+            disp('')
+        end
+        titleabove( { ['(Submap) win = ' num2str(WinSz)], ['fs = ' num2str(SampleRate)]})
+
+        saveas(fig_rms,strrep(RMSFigFilePath,'.png','-SubMap.png'));
+        
+        %% Run Hopper
+        H = HopperSVM(...
+            'Data', HData, ...
+            'MetaData', HLabels, ...
+            'ClassMap', ClassMap,...
+            'Groups', HGroups,...
+            'GroupMap',GroupMap,...
+            'FeatureNames',Feat, ...
+            'Options', Options, ...
+            'ModelNames', ModelType,...
+            'UseParallel', UseParallel);
+        tic
+        H = H.H_FEATURE_EXTRACTORS();
+        toc
+        save(ModelFilePath, 'H','-v7.3')
+        clear HData HLabels HGroups GroupMap
+        
+        % Train - Resub
+        tic
+        H = H.H_TRAIN_MODELS(1);
+        H = H.H_RESUB();
+        save(ModelFilePath, 'H','-v7.3')
+        syslog(['Total training time = ' num2str(toc/60) ' min'])
+        
+        % Save - HSVM + Resub CM
+        Models = H.HopperModels.Properties.VariableNames;
+        Models = reshape(Models,1,length(Models));
+        Feats = H.HopperModels.Properties.RowNames;
+        Feats = reshape(Feats,1,length(Feats));
+        for M = Models
+            M = char(M);
+            for f = 1:length(Feats)
+                F = Feats{f};
+                % hsvm
+                HSVM = H.HopperModels.(M)(f);
+                HSVMBaseName = [HSVMName '-' strrep(M,'_','-') '-' F];
+                HSVMBaseName = char( join( unique( split( HSVMBaseName,'-' ) ) , '-' ) );
+                HSVMPath = fullfile(hsvmdir,HSVMBaseName);
+                save(HSVMPath, 'HSVM','ClassMap','-v7.3')
+                % resub conf mat
+                ResubConfMatPath = fullfile(confmatdir,[ HSVMBaseName '-ResubCM.png' ] );
+                figure(fig_confmat);clf
+                ResubConfMat = H.HopperModels.(M)(f).ResubConfusionMat;
+                plotConfMat(ResubConfMat,H.ClassMap)
+                titleabove({HSVMBaseName,'Resub'})
+                saveas(gcf,ResubConfMatPath);
+            end
+        end
+        
+        % Train - KFold
+        rng(0);
+        tic
+        H = H.H_KFOLD([], NumGroups, [], UseGroups);
+        syslog(['Total kfold time = ' num2str(toc/60) ' min'])
+        
+        % save Model
+        syslog(['Saving to ' ModelFilePath])
+        save(ModelFilePath, 'H','-v7.3')
+        
+        % Save KFold CM
+        Models = H.HopperModels.Properties.VariableNames;
+        Models = reshape(Models,1,length(Models));
+        Feats = H.HopperModels.Properties.RowNames;
+        Feats = reshape(Feats,1,length(Feats));
+        for M = Models
+            M = char(M);
+            for f = 1:length(Feats)
+                F = Feats{f};
+                % hsvm
+                HSVM = H.HopperModels.(M)(f);
+                HSVMBaseName = [HSVMName '-' strrep(M,'_','-') '-' F];
+                HSVMBaseName = char( join( unique( split( HSVMBaseName,'-' ) ) , '-' ) );
+                HSVMPath = fullfile(hsvmdir,HSVMBaseName);
+                save(HSVMPath, 'HSVM','-v7.3')
+                % kfold conf mat
+                KFoldConfMatPath = fullfile(confmatdir,[ HSVMBaseName '-KFoldCM.png' ] );
+                figure(fig_confmat);clf
+                KFoldConfMat = H.HopperModels.(M)(f).KFoldConfusionMat;
+                plotConfMat(KFoldConfMat,ClassMap)
+                titleabove({HSVMName,'KFold'})
+                saveas(gcf,KFoldConfMatPath);
+            end
+        end
+        
+        %% Save ResultsTable with MetaData
+        syslog(['Saving to ' TrainResultsTableFilePath])
+        ResultsTable = getClassifyResultsTable(H.HopperModels);
+        writetable(ResultsTable,TrainResultsTableFilePath)
+        
+        %% Test Hopper
+        disp('')
+        
+        %% end loop feat
     end
-
-     %% Save ResultsTable with MetaData
-    syslog(['Saving to ' TrainResultsTableFilePath])
-    ResultsTable = getClassifyResultsTable(H.HopperModels);
-    writetable(ResultsTable,TrainResultsTableFilePath)
-    
-    %% Test Hopper
-    disp('')
-
     %% end loop windows
 end
-
-%% Emd of script
+%% End of script
 syslog('Script complete.','x')
